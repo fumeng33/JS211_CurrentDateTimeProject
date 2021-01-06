199cd22296d53e3d6e21ba169d47cd4c90a9899d
Meng Fu <Ellie@Ellies-Air.lan1>
2021-01-05 21:54:33 -0600

    completedexamples

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 // **THIS IS INCREDIBLY IMPORTANT THAT YOU DO BOTH SECTIONS!!! You will be doing only front-end work in 421 and you need to brush up on your HTML elements**
 
+// const { ResourceLoader } = require("jsdom");
+
 
 // ***************************
 //          PART ONE
@@ -16,35 +18,113 @@ const displayDate = () => {
 
 // Write a JavaScript program to convert a number to a string.
 
+let num = 15;
+num.toString();
+console.log(num)
 
 
 // Write a JavaScript program to convert a string to the number.
-
+const parseIntMe =()=> {
+let x = '20'
+parseInt(x)
+console.log(x)
+}
 
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
-  // * Boolean
-  // * Null
-  // * Undefined
-  // * Number
-  // * NaN
-  // * String
+  // * Boolean 
   
+  const dataType = (dog) => {
+      // write a function that takes in a varible and return as it datatype
+
+    return typeof dog;
+  }
+  
+  let birthdayMessage = true
+  console.log(dataType (true))
+
+  let string1 = "cats"
+  console.log(dataType(string1))
+//   // * Null
+
+  let blackhole = null
+  console.log(dataType(blackhole))
+
+  console.log(dataType(parseIntMe))
+
+//   // * Undefined
+let water 
+console.log(dataType(water))
+
+//   // * Number
+let height = 5
+let weight = 2
+
+console.log(dataType(height))
+console.log(dataType(weight))
+
+
+
+// * NaN
+function myX(k) {
+  if (isNaN(k)) {
+    return NaN;
+  }
+  return k;
+}
+console.log(myX('NotANumber'))
+
+//   // * String
+let str = "Hello";
+let str2 = 'Single quotes are ok too';
+let str3= `hello from ${str2}`;
+console.log (str3)
 
   
-// Write a JavaScript program that adds 2 numbers together.
+// // Write a JavaScript program that adds 2 numbers together.
 
+let num1= 5;
+let num2 = 5;
 
-
+const sumNum= num1+ num2
+console.log(sumNum)
 // Write a JavaScript program that runs only when 2 things are true.
+const textNum =(a) => {
+  if (a > 0 && a < 10) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
+console.log(textNum(11));
 
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
+const textNum1 =(a) => {
+  if (a > 0 || a < 10) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
+console.log(textNum(9));
 
 
 // Write a JavaScript program that runs when both things are not true.  
+const textNum2 =(a) => {
+  if (a > 0 && a < 10) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+console.log(textNum(-3));
 
 // ***************************
 //         PART TWO
@@ -65,3 +145,7 @@ const displayDate = () => {
 // Video1: https://player.vimeo.com/video/377147232
 // Video2: https://www.youtube.com/embed/bkvH28PXLWc
 // Video3: https://www.youtube.com/embed/TrGI9Yki-24
+
+// Meng - DateAndTime
+// body: link to get your code ( github, repit, paste code)
+// all assignments are one week from the day that got assigned to 
